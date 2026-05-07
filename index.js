9750b5e375e1ab4cd42e2fce2d26e1ec68709911
ucheeke75@gmail.com <ucheeke75@gmail.com>
2026-05-07 13:12:40 +0100

    Another change on index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 
-  res.send('🚀 DevOps Capstone App is Running perfectly and CI/CD is working!');
+  res.send('🚀 New DevOps Capstone App is Running perfectly and CI/CD is working!');
 
 });
 
